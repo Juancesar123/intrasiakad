0a4d7ee395de2bdb9dd4821b23f8731c9a15c7df
bagas <bagassigit24@gmail.com>
2018-10-23 10:32:24 +0700

    Get Fungction data pengajar
    -

--- Modules/Dataguru/Http/Controllers/DataguruController.php
+++ Modules/Dataguru/Http/Controllers/DataguruController.php
@@ -17,7 +17,16 @@ class DataguruController extends Controller
      */
     public function index()
     {
-        return view('dataguru::index');
+      $token = session()->get('token');
+      $client = new Client();
+      $token_decode = json_decode($token)->accessToken;
+      $get_data=$client->request('GET',env('API_URL').'/datapengajar/', [
+             'headers' => [
+                      'Authorization'     => $token_decode
+                    ]
+          ]);
+      $datapengajar = json_decode($get_data->getBody()->getContents());
+    return view('dataguru::index',compact('dataguru'));
     }
 
     /**
