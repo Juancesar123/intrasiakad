97419199b836ada0daabd54c9615b808ab58b7e6
Dwi Adi Tiyo <tiyoadidwi@gmail.com>
2018-11-01 22:39:33 +0700

    fixin route

--- Modules/Dataguru/Http/routes.php
+++ Modules/Dataguru/Http/routes.php
@@ -3,7 +3,7 @@
 Route::group(['middleware' => ['web','token'], 'prefix' => 'dataguru', 'namespace' => 'Modules\Dataguru\Http\Controllers'], function()
 {
     Route::get('/', 'DataguruController@index')->name('indexDataguru');
-    Route::get('/createdataguru', 'DataguruController@create');
+    Route::get('/createdataguru', 'DataguruController@create')->name('tambahguru');
     Route::get('/edit/{id}', 'DataguruController@edit')->name('editform');
     Route::put('/update/{id}', 'DataguruController@update');
     Route::get('/delete/{id}', 'DataGuruController@destroy');
