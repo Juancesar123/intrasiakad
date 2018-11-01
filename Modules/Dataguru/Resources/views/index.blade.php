b48b598595563454baa62f2b065ceda1758d8837
ferdian666 <muhamadferdian539@gmail.com>
2018-11-01 10:57:50 +0700

    /createformjadwal: gitflow-feature-stash: jadwalpelajaran

--- Modules/Dataguru/Resources/views/index.blade.php
+++ Modules/Dataguru/Resources/views/index.blade.php
@@ -16,43 +16,44 @@
           <h3 class="box-title">Tabel Data Guru</h3>
 
           <div class="row">
-          <div class="col-md-4"></div>
-          <div class="form-group col-md-4">
+          <div class="col-md-1"></div>
+          <div class="form-group col-md-6">
             <label for="Name">Name:</label>
             <input type="text" class="form-control" name="name">
           </div>
         </div>
         <div class="row">
-          <div class="col-md-4"></div>
-            <div class="form-group col-md-4">
+          <div class="col-md-1"></div>
+          <div class="form-group col-md-6">
+            <label for="Address">Address:</label>
+            <input type="text" class="form-control" name="Address">
+          </div>
+        </div>
+        <div class="row">
+          <div class="col-md-1"></div>
+            <div class="form-group col-md-6">
               <label for="Email">Email:</label>
               <input type="text" class="form-control" name="email">
             </div>
           </div>
         <div class="row">
-          <div class="col-md-4"></div>
-            <div class="form-group col-md-4">
+          <div class="col-md-1"></div>
+            <div class="form-group col-md-6">
               <label for="Number">Phone Number:</label>
               <input type="text" class="form-control" name="number">
             </div>
           </div>
-        <div class="row">
-          <div class="col-md-4"></div>
-          <div class="form-group col-md-4">
-            <input type="file" name="filename">    
-         </div>
-        </div>
-        <div class="row">
-          <div class="col-md-4"></div>
-          <div class="form-group col-md-4">
-            <strong>Date : </strong>  
-            <input class="date form-control"  type="text" id="datepicker" name="date">   
-         </div>
+          <div class="row">
+          <div class="col-md-1"></div>
+          <div class="form-group col-md-6">
+            <label for="Birt of Date">Birt of Date:</label>
+            <input type="text" class="form-control" name="name">
+          </div>
         </div>
          <div class="row">
-          <div class="col-md-4"></div>
-            <div class="form-group col-md-4">
-                <lable>Jenis Kelamin</lable>
+          <div class="col-md-1"></div>
+            <div class="form-group col-md-6">
+                <label>Gender</label>
                 <select name="office">
                   <option value="">Chose</option>
                   <option value="Laki-laki">Laki-laki</option>
@@ -61,8 +62,8 @@
             </div>
         </div>
         <div class="row">
-          <div class="col-md-4"></div>
-          <div class="form-group col-md-4" style="margin-top:60px">
+          <div class="col-md-1"></div>
+          <div class="form-group col-md-6" style="margin-top:60px">
             <button type="submit" class="btn btn-success">Submit</button>
           </div>
         </div>
--- Modules/JadwalPelajaran/Resources/views/index.blade.php
+++ Modules/JadwalPelajaran/Resources/views/index.blade.php
@@ -1,9 +1,44 @@
-@extends('jadwalpelajaran::layouts.master')
+@extends('app.layouts')
 
 @section('content')
-    <h1>Hello World</h1>
+<section class="content-header">
+  <h1>
+    Data Jadwal
+    <small>Menu</small>
+  </h1>
+</section>
 
-    <p>
-        This view is loaded from module: {!! config('jadwalpelajaran.name') !!}
-    </p>
+<section class="content">
+  <div class="row">
+    <div class="col-xs-12">
+      <div class="box">
+        <div class="box-header">
+          <h3 class="box-title">Tabel Data Jadwal</h3>
+          <div class="row">
+          <div class="col-md-1"></div>
+          <div class="form-group col-md-6">
+            <label for="Name">Kode Mata Pelajaran:</label>
+            <input type="text" class="form-control" name="name">
+          </div>
+        </div>
+        <div class="row">
+          <div class="col-md-1"></div>
+          <div class="form-group col-md-6">
+            <label for="Name">Name Guru:</label>
+            <input type="text" class="form-control" name="name">
+          </div>
+        </div>
+        <div class="row">
+          <div class="col-md-1"></div>
+          <div class="form-group col-md-6">
+            <label for="Name">Name Pelajaran:</label>
+            <input type="text" class="form-control" name="name">
+          </div>
+        </div>
+        <div class="row">
+            <div class="col-md-1"></div>
+                <div class="form-group col-md-6" style="margin-top:60px">
+                <button type="submit" class="btn btn-success">Submit</button>
+              </div>
+          </div>                 
 @stop
