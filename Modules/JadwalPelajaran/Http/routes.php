e7647b353bd69c8e1eb97f912ef3494237252942
juancesar123 <juancesarandrianto@gmail.com>
2018-11-01 15:05:36 +0700

    Auto stash before merge of "develop" and "origin/develop"

--- Modules/JadwalPelajaran/Http/routes.php
+++ Modules/JadwalPelajaran/Http/routes.php
@@ -1,9 +1,8 @@
 <?php
 
-Route::group(['middleware' => 'web', 'prefix' => 'jadwalpelajaran', 'namespace' => 'Modules\JadwalPelajaran\Http\Controllers'], function()
+Route::group(['middleware' => 'web', 'prefix' => 'jadwalkursus', 'namespace' => 'Modules\JadwalPelajaran\Http\Controllers'], function()
 {
-
-    Route::get('/', 'JadwalPelajaranController@index')->name('jadwalindex');
+    Route::get('/', 'JadwalPelajaranController@index')->name('jadwalkursus');
     Route::delete('/delete/{id}', 'JadwalPelajaranController@destroy');
     Route::get('/edit/{id}', 'JadwalPelajaranController@edit')->name('jadwalformedit');
     Route::patch('/update', 'JadwalPelajaranController@update')->name('jadwalupdate');
