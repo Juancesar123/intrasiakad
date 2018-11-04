0abb9209ced8020f2a164f8fd28da65e4b655942
Dwi Adi Tiyo <tiyoadidwi@gmail.com>
2018-11-04 22:48:02 +0700

    form create data peserta

--- Modules/Datasiswa/Http/routes.php
+++ Modules/Datasiswa/Http/routes.php
@@ -3,4 +3,5 @@
 Route::group(['middleware' => 'web', 'prefix' => 'datasiswa', 'namespace' => 'Modules\Datasiswa\Http\Controllers'], function()
 {
     Route::get('/', 'DatasiswaController@index');
+    Route::get('/createPeserta', 'DatasiswaController@create');
 });
